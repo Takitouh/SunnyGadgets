affe7b7da2284ca0653e758f87bdd8e3df80a23c
Takitouh <1097097476jhon@gmail.com>
2025-06-26 18:16:30 -0500

    refactor: rename from User to UserSec

--- src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/dto/UserSecCreateDTO.java
+++ src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/dto/UserSecCreateDTO.java
@@ -1,7 +1,5 @@
 package com.SunnyGadgetsProject.SunnyGadgets_v1.dto;
 
-import com.SunnyGadgetsProject.SunnyGadgets_v1.entity.Role;
-import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
@@ -16,7 +14,7 @@ import java.util.Set;
 
 @Getter @Setter @NoArgsConstructor
 @AllArgsConstructor
-public class UserCreateDTO {
+public class UserSecCreateDTO {
     @NotBlank(message = "Username can't be blank or null")
     private String username;
     @NotBlank(message = "Password can't be blank or null")
