f8f3a5ae861fde18beb6909caa881ae608f49436
Takitouh <1097097476jhon@gmail.com>
2025-06-25 20:10:13 -0500

    added atributes commision and hireDate to Seller

--- src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/entity/Seller.java
+++ src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/entity/Seller.java
@@ -3,6 +3,7 @@ package com.SunnyGadgetsProject.SunnyGadgets_v1.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Date;
 import java.util.Set;
 
 @Entity @Getter
@@ -16,4 +17,8 @@ public class Seller extends Employee {
 
     @OneToMany(mappedBy = "seller")
     private Set<Sale> sales;
+
+    private long commision;
+
+    private Date hireDate;
 }
