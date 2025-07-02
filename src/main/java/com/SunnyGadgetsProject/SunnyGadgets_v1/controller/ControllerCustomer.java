11a10064559ce152821a884d614f87272018dd1c
Takitouh <1097097476jhon@gmail.com>
2025-07-01 20:09:29 -0500

    feature: configured controllerCustomer for the use of DTO

--- src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/controller/ControllerCustomer.java
+++ src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/controller/ControllerCustomer.java
@@ -1,8 +1,9 @@
 package com.SunnyGadgetsProject.SunnyGadgets_v1.controller;
 
+import com.SunnyGadgetsProject.SunnyGadgets_v1.dto.CustomerCreateDTO;
+import com.SunnyGadgetsProject.SunnyGadgets_v1.dto.CustomerResponseDTO;
 import com.SunnyGadgetsProject.SunnyGadgets_v1.entity.Customer;
 import com.SunnyGadgetsProject.SunnyGadgets_v1.service.IServiceCustomer;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -10,7 +11,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @EnableMethodSecurity(prePostEnabled = true)
@@ -19,44 +19,35 @@ import java.util.Optional;
 public class ControllerCustomer {
     private final IServiceCustomer serviceCustomer;
 
+
     public ControllerCustomer(IServiceCustomer serviceCustomer) {
         this.serviceCustomer = serviceCustomer;
     }
 
     @GetMapping("/get/{id}")
     @PreAuthorize("hasAuthority('READ')")
-    public ResponseEntity<Customer> getCustomer(@PathVariable Long id) {
-        Optional<Customer> customerOptional = serviceCustomer.getCustomerById(id);
-        if (customerOptional.isEmpty()){
-            throw new EntityNotFoundException("Customer with ID " + id + " not found");
-        }
-        return ResponseEntity.ok(customerOptional.get());
+    public ResponseEntity<CustomerResponseDTO> getCustomer(@PathVariable Long id) {
+        return ResponseEntity.ok(serviceCustomer.getCustomerById(id));
+
     }
 
     @GetMapping("/get")
     @PreAuthorize("hasAuthority('READ')")
-    public ResponseEntity<List<Customer>> getAllCustomers() {
-        List<Customer> customers = serviceCustomer.allCustomers();
-        if (customers.isEmpty()){
-            throw new EntityNotFoundException("Customer list is empty");
-        }
-        return ResponseEntity.ok(customers);
+    public ResponseEntity<List<CustomerResponseDTO>> getAllCustomers() {
+        return ResponseEntity.ok(serviceCustomer.allCustomers());
+
     }
 
     @PostMapping("/create")
     @PreAuthorize("hasAuthority('CREATE')")
-    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
-        serviceCustomer.createCustomer(customer);
-
-        return new ResponseEntity<>(customer, HttpStatus.CREATED);
+    public ResponseEntity<CustomerResponseDTO> createCustomer(@RequestBody CustomerCreateDTO customer) {
+        return new ResponseEntity<>(serviceCustomer.createCustomer(customer), HttpStatus.CREATED);
     }
 
     @PostMapping("/createBatch")
     @PreAuthorize("hasAuthority('CREATE')")
-    public ResponseEntity<List<Customer>> createCustomer(@RequestBody List<Customer> customers) {
-        serviceCustomer.createCustomer(customers);
-
-        return new ResponseEntity<>(customers, HttpStatus.CREATED);
+    public ResponseEntity<List<CustomerResponseDTO>> createCustomer(@RequestBody List<CustomerCreateDTO> customers) {
+        return new ResponseEntity<>(serviceCustomer.createCustomer(customers), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete/{id}")
