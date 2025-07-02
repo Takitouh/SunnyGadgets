0036697481539b26107d5b69fe7519c2479cde6c
Takitouh <1097097476jhon@gmail.com>
2025-07-01 20:10:14 -0500

    feature: configured ControllerRole for the use of DTO

--- src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/controller/ControllerRole.java
+++ src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/controller/ControllerRole.java
@@ -1,15 +1,14 @@
 package com.SunnyGadgetsProject.SunnyGadgets_v1.controller;
 
-import com.SunnyGadgetsProject.SunnyGadgets_v1.entity.Permission;
-import com.SunnyGadgetsProject.SunnyGadgets_v1.entity.Role;
+import com.SunnyGadgetsProject.SunnyGadgets_v1.dto.RoleCreateDTO;
+import com.SunnyGadgetsProject.SunnyGadgets_v1.dto.RoleResponseDTO;
 import com.SunnyGadgetsProject.SunnyGadgets_v1.service.IServicePermission;
 import com.SunnyGadgetsProject.SunnyGadgets_v1.service.IServiceRole;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 @RestController
 @RequestMapping("/api/v1/roles")
@@ -24,58 +23,58 @@ public class ControllerRole {
     }
 
     @GetMapping("/get")
-    public ResponseEntity<List<Role>> getAllRoles() {
-        List<Role> roles = roleService.allRoles();
-        return ResponseEntity.ok(roles);
+    public ResponseEntity<List<RoleResponseDTO>> getAllRoles() {
+        return ResponseEntity.ok(roleService.allRoles());
     }
 
     @GetMapping("/get/{id}")
-    public ResponseEntity<Role> getRoleById(@PathVariable Long id) {
-        Optional<Role> role = roleService.getRoleById(id);
-        return role.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    public ResponseEntity<RoleResponseDTO> getRoleById(@PathVariable Long id) {
+        return ResponseEntity.ok(roleService.getRoleById(id));
     }
 
     @PostMapping("/create")
-    public ResponseEntity<Role> createRole(@RequestBody Role role) {
-        Set<Permission> permissionList = new HashSet<>();
-        Permission readPermission;
-
-        // Recuperar la Permission/s por su ID
-        for (Permission per : role.getPermissions()) {
-            readPermission = permissionService.getPermissionById(per.getId()).orElse(null);
-            if (readPermission != null) {
-                //si encuentro, guardo en la lista
-                permissionList.add(readPermission);
-            }
-        }
-
-        role.setPermissions(permissionList);
-
-        System.out.println(role);
-        Role newRole = roleService.createRole(role);
-        return ResponseEntity.ok(newRole);
+    public ResponseEntity<RoleResponseDTO> createRole(@RequestBody RoleCreateDTO role) {
+//        Set<Permission> permissionList = new HashSet<>();
+//        Permission readPermission;
+//
+//        // Recuperar la Permission/s por su ID
+//        for (Permission per : role.getPermissions()) {
+//            readPermission = permissionService.getPermissionById(per.getId()).orElse(null);
+//            if (readPermission != null) {
+//                //si encuentro, guardo en la lista
+//                permissionList.add(readPermission);
+//            }
+//        }
+//
+//        role.setPermissions(permissionList);
+//
+//        Role newRole = roleService.createRole(role);
+//        return ResponseEntity.ok(newRole);
+        return new ResponseEntity<>(roleService.createRole(role), HttpStatus.CREATED);
     }
 
     @PostMapping("/createBatch")
-    public ResponseEntity<List<Role>> createRole(@RequestBody Set<Role> role) {
-        Set<Permission> permissionSet = new HashSet<>();
-        Permission readPermission;
+    public ResponseEntity<List<RoleResponseDTO>> createRole(@RequestBody Set<RoleCreateDTO> role) {
+//        Set<Permission> permissionSet = new HashSet<>();
+//        Permission readPermission;
+//
+//        // Recuperar la Permission/s por su ID
+//        for (Role ro : role) {
+//            for (Permission per : ro.getPermissions()) {
+//                readPermission = permissionService.getPermissionById(per.getId()).orElse(null);
+//                if (readPermission != null) {
+//                    //si encuentro, guardo en la lista
+//                    permissionSet.add(readPermission);
+//                }
+//            }
+//
+//
+//            ro.setPermissions(permissionSet);
+//        }
+//
+//        List<Role> newRole = roleService.createRole(role);
+//        return ResponseEntity.ok(newRole);
 
-        // Recuperar la Permission/s por su ID
-        for (Role ro : role) {
-            for (Permission per : ro.getPermissions()) {
-                readPermission = permissionService.getPermissionById(per.getId()).orElse(null);
-                if (readPermission != null) {
-                    //si encuentro, guardo en la lista
-                    permissionSet.add(readPermission);
-                }
-            }
-
-
-            ro.setPermissions(permissionSet);
-        }
-
-        List<Role> newRole = roleService.createRole(role);
-        return ResponseEntity.ok(newRole);
+        return ResponseEntity.ok(roleService.createRole(role));
     }
 }
