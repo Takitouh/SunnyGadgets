75e6a2380208d83fcbb9aabd901706ee9272fcbc
Takitouh <1097097476jhon@gmail.com>
2025-07-09 20:41:17 -0500

    feature: implemented methods for handle other types of exceptions

--- src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/exception/GlobalHandlerException.java
+++ src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/exception/GlobalHandlerException.java
@@ -15,5 +15,28 @@ public class GlobalHandlerException {
         ErrorDTO err = new ErrorDTO("NOT_FOUND_404", ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ErrorDTO> handleNoHandlerFound(NoHandlerFoundException ex) {
+        ErrorDTO err = new ErrorDTO("ENDPOINT_NOT_FOUND", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorDTO> handleAccessDenied(AccessDeniedException ex) {
+        ErrorDTO err = new ErrorDTO("ACCESS_DENIED", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorDTO> handleAuthentication(AuthenticationException ex) {
+        ErrorDTO err = new ErrorDTO("AUTHENTICATION_FAILED", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(err);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDTO> handleException(Exception ex) {
+        ErrorDTO err = new ErrorDTO("SOMETHING WENT WRONG", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(err);
+    }
 
 }
