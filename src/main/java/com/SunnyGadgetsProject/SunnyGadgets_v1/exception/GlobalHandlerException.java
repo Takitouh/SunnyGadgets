57a6ca8771833d0f6503e9437bfde9ec132f2fe3
Takitouh <1097097476jhon@gmail.com>
2025-07-09 20:43:29 -0500

    refactor: added code exception to ExceptionHandlers

--- src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/exception/GlobalHandlerException.java
+++ src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/exception/GlobalHandlerException.java
@@ -6,36 +6,40 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import javax.naming.AuthenticationException;
+import java.nio.file.AccessDeniedException;
 
 @RestControllerAdvice
 public class GlobalHandlerException {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorDTO> handleEntityNotFound(EntityNotFoundException ex) {
-        ErrorDTO err = new ErrorDTO("NOT_FOUND_404", ex.getMessage());
+        ErrorDTO err = new ErrorDTO("ENTITY_NOT_FOUND_404", ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
     @ExceptionHandler(NoHandlerFoundException.class)
     public ResponseEntity<ErrorDTO> handleNoHandlerFound(NoHandlerFoundException ex) {
-        ErrorDTO err = new ErrorDTO("ENDPOINT_NOT_FOUND", ex.getMessage());
+        ErrorDTO err = new ErrorDTO("ENDPOINT_NOT_FOUND_404", ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<ErrorDTO> handleAccessDenied(AccessDeniedException ex) {
-        ErrorDTO err = new ErrorDTO("ACCESS_DENIED", ex.getMessage());
+        ErrorDTO err = new ErrorDTO("ACCESS_DENIED_403", ex.getMessage());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
     }
 
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<ErrorDTO> handleAuthentication(AuthenticationException ex) {
-        ErrorDTO err = new ErrorDTO("AUTHENTICATION_FAILED", ex.getMessage());
+        ErrorDTO err = new ErrorDTO("AUTHENTICATION_FAILED_401", ex.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(err);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorDTO> handleException(Exception ex) {
-        ErrorDTO err = new ErrorDTO("SOMETHING WENT WRONG", ex.getMessage());
+        ErrorDTO err = new ErrorDTO("INTERNAL_ERROR_500", ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(err);
     }
 
