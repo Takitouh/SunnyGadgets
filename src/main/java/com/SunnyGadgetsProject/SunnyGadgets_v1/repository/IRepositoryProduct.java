a194dd2624d24f6403248e5f81aa209bdc2b6faf
Takitouh <1097097476jhon@gmail.com>
2025-07-15 18:22:09 -0500

    feature: implemented method for handle query of Product to find according to price

--- src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/repository/IRepositoryProduct.java
+++ src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/repository/IRepositoryProduct.java
@@ -1,8 +1,12 @@
 package com.SunnyGadgetsProject.SunnyGadgets_v1.repository;
 
+import com.SunnyGadgetsProject.SunnyGadgets_v1.dto.NameDescriptionPriceProductDTO;
+import com.SunnyGadgetsProject.SunnyGadgets_v1.dto.ProductResponseDTO;
 import com.SunnyGadgetsProject.SunnyGadgets_v1.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IRepositoryProduct extends JpaRepository<Product, Long> {
