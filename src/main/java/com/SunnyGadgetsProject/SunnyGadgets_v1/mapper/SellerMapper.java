4bdaa2cb781a06dbecd7f90f848558918eff475a
Takitouh <1097097476jhon@gmail.com>
2025-07-18 18:47:02 -0500

    refactor: Converted SellerMapper from abstract class to interface because it didn't need to use any other Beans

--- src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/mapper/SellerMapper.java
+++ src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/mapper/SellerMapper.java
@@ -2,19 +2,12 @@ package com.SunnyGadgetsProject.SunnyGadgets_v1.mapper;
 
 import com.SunnyGadgetsProject.SunnyGadgets_v1.dto.SellerCreateDTO;
 import com.SunnyGadgetsProject.SunnyGadgets_v1.dto.SellerResponseDTO;
-import com.SunnyGadgetsProject.SunnyGadgets_v1.entity.Sale;
 import com.SunnyGadgetsProject.SunnyGadgets_v1.entity.Seller;
-import com.SunnyGadgetsProject.SunnyGadgets_v1.repository.IRepositorySale;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.beans.factory.annotation.Autowired;
 
-@Mapper(componentModel = "spring", uses = {SaleMapper.class})
-
-public abstract class SellerMapper {
-
-    @Autowired
-    protected IRepositorySale repositorySale;
+@Mapper(componentModel = "spring")
+public interface SellerMapper {
     // CreateDTO → Entity
     @Mapping(target = "idEmployee", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
@@ -24,11 +17,6 @@ public abstract class SellerMapper {
 
     // Entity → ResponseDTO
     @Mapping(target = "idSeller", source = "idEmployee")
-    @Mapping(target = "createdAt", source = "createdAt")
+    SellerResponseDTO toDto(Seller seller);
 
-    public abstract SellerResponseDTO toDto(Seller seller);
-    @SuppressWarnings("unused")
-    public Sale saleDTOtoSale(Long id){
-        return repositorySale.findById(id).orElse(new Sale());
-    }
 }
