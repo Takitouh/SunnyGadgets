7a8d2629dbdf7a9a1de1ad70c9f2e73c25e11adb
Takitouh <1097097476jhon@gmail.com>
2025-07-22 10:52:21 -0500

    Feature: Added method authenticate and loginUser to use JWT

--- src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/security/config/UserDetailsServiceImp.java
+++ src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/security/config/UserDetailsServiceImp.java
@@ -1,25 +1,40 @@
 package com.SunnyGadgetsProject.SunnyGadgets_v1.security.config;
 
 
+import com.SunnyGadgetsProject.SunnyGadgets_v1.dto.AuthLoginDTO;
+import com.SunnyGadgetsProject.SunnyGadgets_v1.dto.AuthResponseDTO;
 import com.SunnyGadgetsProject.SunnyGadgets_v1.entity.UserSec;
 import com.SunnyGadgetsProject.SunnyGadgets_v1.repository.IRepositoryUserSec;
+import com.SunnyGadgetsProject.SunnyGadgets_v1.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class UserDetailsServiceImp implements UserDetailsService {
 
     @Autowired
     private IRepositoryUserSec userRepo;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername (String username) throws UsernameNotFoundException {
@@ -53,5 +68,24 @@ public class UserDetailsServiceImp implements UserDetailsService {
                 authorityList);
     }
 
+
+    public AuthResponseDTO loginUser(AuthLoginDTO userRequest){
+        String username = userRequest.username();
+        String password = userRequest.password();
+        Authentication authentication = this.authenticate(username, password);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String accessToken = jwtUtils.createToken(authentication);
+        AuthResponseDTO authResponseDTO = new AuthResponseDTO(username, "Login succesful", accessToken, true);
+        return authResponseDTO;
+    }
+
+    private Authentication authenticate(String username, String password) {
+        UserDetails userDetails = this.loadUserByUsername(username);
+
+        if(!Objects.equals(userDetails.getUsername(), username) || !passwordEncoder.matches(password, userDetails.getPassword())) {
+            throw new BadCredentialsException("Invalid username or password");
+        }
+       return new UsernamePasswordAuthenticationToken(username, userDetails.getPassword(), userDetails.getAuthorities());
+    }
 }
 
