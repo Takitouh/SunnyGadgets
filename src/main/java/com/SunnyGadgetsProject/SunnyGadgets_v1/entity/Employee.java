daf866223633ae7375f2d503085657240f479756
Takitouh <1097097476jhon@gmail.com>
2025-07-27 18:23:24 -0500

    refactor: changed inherit strategy to @MappedSuperClass

--- src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/entity/Employee.java
+++ src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/entity/Employee.java
@@ -2,19 +2,14 @@ package com.SunnyGadgetsProject.SunnyGadgets_v1.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.sql.Timestamp;
-
-@Entity
-@Inheritance(strategy = InheritanceType.JOINED)
-@Getter @Setter @NoArgsConstructor
-@Table(name = "employes")
-
-public class Employee{
+@Getter @Setter
+@MappedSuperclass
+public abstract class Employee{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idEmployee;
