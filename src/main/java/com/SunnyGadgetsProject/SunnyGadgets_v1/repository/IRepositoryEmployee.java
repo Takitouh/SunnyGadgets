6283422b0ba21e0327eb661024b21b83cc2a7718
Takitouh <1097097476jhon@gmail.com>
2025-07-27 17:36:43 -0500

    refactor: deleted Employee and it's utilities because wasn't very useful

--- src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/repository/IRepositoryEmployee.java
+++
@@ -1,9 +0,0 @@
-package com.SunnyGadgetsProject.SunnyGadgets_v1.repository;
-
-import com.SunnyGadgetsProject.SunnyGadgets_v1.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IRepositoryEmployee extends JpaRepository<Employee, Long> {
-}
