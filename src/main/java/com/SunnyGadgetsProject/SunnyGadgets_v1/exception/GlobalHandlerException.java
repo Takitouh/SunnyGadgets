b8b04d881815d50a2dce27260f334181059d344d
Takitouh <1097097476jhon@gmail.com>
2025-07-31 13:26:09 -0500

    feature: added new @ExceptionHandler for missing parameter

--- src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/exception/GlobalHandlerException.java
+++ src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/exception/GlobalHandlerException.java
@@ -4,6 +4,7 @@ import com.SunnyGadgetsProject.SunnyGadgets_v1.dto.ErrorDTO;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
@@ -35,6 +36,12 @@ public class GlobalHandlerException {
     public ResponseEntity<ErrorDTO> handleAuthentication(AuthenticationException ex) {
         ErrorDTO err = new ErrorDTO("AUTHENTICATION_FAILED_401", ex.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(err);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ErrorDTO> handleExceptionRequestParameter(Exception ex) {
+        ErrorDTO err = new ErrorDTO("MISSING_PARAMETER_400", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(err);
     }
 
     @ExceptionHandler(Exception.class)
