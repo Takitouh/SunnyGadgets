99de7b70e74e042abc4432cc0a9c3c9aab7258da
Takitouh <1097097476jhon@gmail.com>
2025-08-18 15:51:20 -0500

    documented POST from AuthenticationController

--- src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/controller/AuthenticationController.java
+++ src/main/java/com/SunnyGadgetsProject/SunnyGadgets_v1/controller/AuthenticationController.java
@@ -2,7 +2,15 @@ package com.SunnyGadgetsProject.SunnyGadgets_v1.controller;
 
 import com.SunnyGadgetsProject.SunnyGadgets_v1.dto.AuthLoginDTO;
 import com.SunnyGadgetsProject.SunnyGadgets_v1.dto.AuthResponseDTO;
+import com.SunnyGadgetsProject.SunnyGadgets_v1.dto.CategoryResponseDTO;
+import com.SunnyGadgetsProject.SunnyGadgets_v1.dto.ErrorDTO;
 import com.SunnyGadgetsProject.SunnyGadgets_v1.security.config.UserDetailsServiceImp;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +26,30 @@ public class AuthenticationController {
     @Autowired
     private UserDetailsServiceImp userDetailsService;
 
+    @Operation(method = "POST", summary = "Request a JWT with username and password")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "JWT sent", content =
+            @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = CategoryResponseDTO.class), examples = {
+                    @ExampleObject(value = """
+                            {
+                                "username" : "Jhon",
+                                "password" : "12345"
+                            }
+                            """)
+            })),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Invalid user or password", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorDTO.class), examples = {@ExampleObject(value = """
+                    {
+                        "code": "INTERNAL_ERROR_500",
+                        "message": "Invalid username or password"
+                    }
+                    """)})
+            )
+    })
     @PostMapping("/login")
-    public ResponseEntity<AuthResponseDTO> login(@RequestBody @Valid AuthLoginDTO userRequest){
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody @Valid AuthLoginDTO userRequest) {
         return ResponseEntity.ok().body(userDetailsService.loginUser(userRequest));
     }
 }
